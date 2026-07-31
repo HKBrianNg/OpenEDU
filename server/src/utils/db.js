a5db637f29b16de45064dda48d3b44a1175222c5
HKBrianNg <ngshunchiang@hotmail.com>
2026-07-31 20:52:39 +0800

    prepare production environment

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 pool.on('error', (err) => {

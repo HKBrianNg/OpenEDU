77861179f4b126d1ee158e874fd6973dcea4bde8
Brian Ng <ngshunchiang@hotmail.com>
2026-08-20 09:03:53 +0800

    add  game

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Typography } from 'antd';
+import InlineGame from '../components/InlineGame';
+
+const { Title, Paragraph } = Typography;
 
 const Home: React.FC = () => {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>欢迎来到 OpenEDU 前台</h1>
-      这里是我们的首页，后续会放轮播图、推荐课程、用户登录入口等。
+    <div style={{ padding: '40px 20px', maxWidth: 1270, margin: '0 auto' }}>
+      <Title level={2}>欢迎来到 OpenEDU</Title>
+      <Paragraph>
+        在这里你可以学习各种有趣的知识，也可以玩玩小游戏放松一下。
+      </Paragraph>
+      <div style={{ marginTop: 30 }}>
+        <Title level={4}>🎮 打飞机小游戏</Title>
+        <InlineGame />
+      </div>
+      {/* 其他首页内容可放在下方 */}
     </div>
   );
 };

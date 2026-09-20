148ad1b6f544f78d07c2bf6f48510bd0cf2438e8
Brian Ng <ngshunchiang@hotmail.com>
2026-09-20 19:21:40 +0800

    remove all course related stuff

--- client/src/pages/CourseDetail/utils.ts
+++
@@ -1,5 +0,0 @@
-export function getLocalizedValue(value: any, locale: string): string {
-  if (!value) return '';
-  if (typeof value === 'string') return value;
-  return value[locale] || value.zh || '';
-}

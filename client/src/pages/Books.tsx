ce89354203b5392ef6bc3796287e5b6ce0ae8f86
Brian Ng <ngshunchiang@hotmail.com>
2026-09-21 20:32:58 +0800

    group translation

--- client/src/pages/Books.tsx
+++ client/src/pages/Books.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import BookFrame from '../components/BookFrame';
-import '../books/JuniorEncyclopedia';
-import '../books/BasicEnglish300';
-import '../books/KnowledgeBase';
 import '../books/EnglishWord';
+import '../books/BasicEnglish300';
+import '../books/JuniorEncyclopedia';
+import '../books/KnowledgeBase';
 
 const Books: React.FC = () => {
   return <BookFrame />;
